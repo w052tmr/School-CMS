13e155950327fac370ffc50b30b89ee51e93ab6d
Troy M. Ricker <email>
2024-09-25 10:46:22 -0400

    Added Response Headers

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,10 @@ app.use(express.json({ limit: "100kb" }));
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-    res.append("Access-Control-Allow-Origin", ["*"]);
+    res.append("Access-Control-Allow-Origin", [
+        "http://localhost:5173",
+        "https://school-cms-client.vercel.app",
+    ]);
     res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
     res.append("Access-Control-Allow-Headers", "Content-Type");
     next();
